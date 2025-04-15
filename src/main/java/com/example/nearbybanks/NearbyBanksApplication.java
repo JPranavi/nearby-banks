12a7ee04baa07678d0435313d1b3f30520e60447
JPranavi <pranavijasthi1024@gmail.com>
2025-04-14 22:04:22 -0500

    Initial commit

---
+++ src/main/java/com/example/nearbybanks/NearbyBanksApplication.java
@@ -0,0 +1,13 @@
+package com.example.nearbybanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyBanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyBanksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nearbybanks/NearbyBanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nearbybanks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyBanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
